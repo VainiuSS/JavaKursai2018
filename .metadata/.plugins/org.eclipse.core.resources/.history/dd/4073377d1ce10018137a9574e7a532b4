1c64b88183b48e017b9aa53827174547f649757e
VainiuSS <vainemania@gmail.com>
2018-11-05 19:02:24 +0200

    qweqw

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/dd/4073377d1ce10018137a9574e7a532b4
@@ -0,0 +1,9 @@
+package puodeliai;
+
+public class Puodelis {
+	protected int maxTalpa;
+	
+	public void rodykInformacija() {
+		System.out.println("Puodelio talpa: " + this.maxTalpa);
+	}
+}
--- Cups/src/puodeliai/Puodelis.java
+++ Cups/src/puodeliai/Puodelis.java
@@ -2,7 +2,7 @@ package puodeliai;
 
 public class Puodelis {
 	protected int maxTalpa;
-	
+	// KOmentaeas commitui
 	public void rodykInformacija() {
 		System.out.println("Puodelio talpa: " + this.maxTalpa);
 	}
